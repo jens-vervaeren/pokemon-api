60fc5bf1d0c830b64b0af44281a894f5d934f0ad
Jens Vervaeren <jens.vervaeren@pau.be>
2024-02-13 20:23:46 +0100

    Config: Enabled api versioning

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from "@nestjs/core"
+import { VersioningType } from "@nestjs/common"
 import { AppModule } from "./modules/app/app.module"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix("api")
+  app.enableVersioning({
+    type: VersioningType.URI
+  })
   console.log("Application listening on port: 3000")
   await app.listen(3000)
 }
