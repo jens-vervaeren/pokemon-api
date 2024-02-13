e34351b72f84f03e46b18767667e3b7f0289f90e
Jens Vervaeren <jens.vervaeren@pau.be>
2024-02-13 17:03:07 +0100

    Config: Setup database connection with Prisma

--- api/src/modules/app/app.module.ts
+++ api/src/modules/app/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    })
+  ],
   controllers: [],
   providers: []
 })
